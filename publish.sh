b750d2d73e6c075a7c3a5576d82afa05ad5c668d
John Kristian <jkristian@linkedin.com>
2011-09-28 11:00:11 -0700

    commentary

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git checkout gh-pages || exit $?
-./publish.sh
+./publish.sh # That's not this script, since we just checked out a different branch.
 git checkout master    || exit $?
 git push github master || exit $?
