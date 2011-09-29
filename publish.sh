2f4450b71e9334dc9f136a05b46efcd58a1f7fb7
John Kristian <jmkristian@gmail.com>
2011-09-29 00:56:28 -0700

    commentary

--- publish.sh
+++ publish.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+# Push javadoc files to a website hosted by github <http://pages.github.com/>.
+# Before executing this script, generate the javadoc files into build/docs/javadoc/.
 git checkout gh-pages || exit $?
-rm -rf javadoc/*
+
+# Clear out the old files:
+rm -rf javadoc/* 
+# Replace them with new files and commit them:
 cp -pr build/docs/javadoc/* javadoc \
 && git add javadoc \
-&& git commit -a -m "update javadoc"
+&& git commit -a -m "generated javadoc"
 ERROR=$?
+
 git checkout master || exit $?
 [ $ERROR -eq 0 ] || exit $ERROR
 git push github master || exit $?
