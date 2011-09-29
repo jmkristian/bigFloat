6e9b759afd17604ab8010c2e12523ca26f50f5d6
John Kristian <jmkristian@gmail.com>
2011-09-28 23:26:21 -0700

    Handle errors more carefully.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git checkout gh-pages || exit $?
-./publish.sh # That's not this script, since we just checked out a different branch.
-git checkout master    || exit $?
+git checkout gh-pages && ./publish.sh; ERROR=$?; git checkout master || exit $?
+# That publish.sh isn't this script; it's a script in the gh-pages branch.
+[ $ERROR -eq 0 ] || exit $ERROR
 git push github master || exit $?
