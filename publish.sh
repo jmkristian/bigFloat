0495b72b45e00527e16353afddf597f51a07ccb4
John Kristian <jmkristian@gmail.com>
2011-09-29 00:32:55 -0700

    Merged publish.sh into master.

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf javadoc/*
-cp -pr build/docs/javadoc/* javadoc || exit $?
-git add javadoc || exit $?
-git commit -a -m "update javadoc" || exit $?
-git push github gh-pages || exit $?
