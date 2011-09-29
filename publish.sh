082538d479c3955be4320790d670b750c5e5edc0
John Kristian <jmkristian@gmail.com>
2011-09-29 00:32:22 -0700

    Merged publish.sh from gh-pages.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-git checkout gh-pages && ./publish.sh; ERROR=$?; git checkout master || exit $?
-# That publish.sh isn't this script; it's a script in the gh-pages branch.
+git checkout gh-pages || exit $?
+rm -rf javadoc/*
+cp -pr build/docs/javadoc/* javadoc \
+&& git add javadoc \
+&& git commit -a -m "update javadoc"
+ERROR=$?
+git checkout master || exit $?
 [ $ERROR -eq 0 ] || exit $ERROR
 git push github master || exit $?
+git push github gh-pages || exit $?
